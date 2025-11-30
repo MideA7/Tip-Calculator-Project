9bb305d2c56440b048dfca887f811f9e943dc6f4
MideA7 <mideakinyele7@gmail.com>
2025-11-30 01:55:05 +0000

    Added javascript. Basic functionality working

---
+++ script.js
@@ -0,0 +1,9 @@
+const amountInput = document.getElementById("amountInput");
+const calcBtn = document.getElementById("calcBtn");
+const tip = document.getElementById("tip");
+
+calcBtn.addEventListener("click", () => {
+  const billAmount = Number(amountInput.value) * 1.15;
+  console.log(billAmount);
+  tip.innerText = `Tip: $${billAmount.toFixed(2)}`;
+});
